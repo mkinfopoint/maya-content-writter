b0c80fc638f357959b904d2a590241f12aa466b6
Praveenram Balachandran <praveenrambalu@gmail.com>
2024-02-29 14:32:55 +0530

    feature: initial-commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import React, { Suspense } from "react";
+import Home from "./screens/Home";
+import { Route, Routes } from "react-router-dom";
+import PreLoader from "./components/PreLoader";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./assets/css/style.css"
+function App() {
+
+  const pageRoute = [
+    {
+      path: "/",
+      element: <Home />
+    }
+  ]
+
+  return (
+    <>
+      <div className="vh-100 auth-layout">
+        <Suspense fallback={<PreLoader />}>
+          <Routes>
+            {pageRoute &&
+              pageRoute.map(({ path, element }, index) => (
+                <Route path={path} element={element} key={index} />
+              ))}
+          </Routes>
+        </Suspense>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,3 @@
+// import all image here
+
+export { }
